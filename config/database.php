5c232076b6315948b2731b3fd36f01686360df35
Indra Adi Baskara <indraadi3@gmail.com>
2026-03-15 12:04:31 +0700

    feat: add database configuration file

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    "host" => "localhost",
+    "port" => 3306,
+    "dbname" => "proweblanjut_crud_a12_2025_07414",
+    "charset" => "utf8mb4",
+    "user" => "root",
+    "pass" => "",
+];
